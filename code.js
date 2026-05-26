445ab984e7aa960464008370519fe0ba72184012
slavoyar-img <yaroslavkozharin@splitinternational.org>
2026-05-26 09:48:40 +0200

    Update code.js

--- code.js
+++ code.js
@@ -1,7 +1,1 @@
-const grid = document.getElementById("grid");
 
-for (let i = 1; i <= 200; i++) {
-  const button = document.createElement("button");
-  button.textContent = i;
-  grid.appendChild(button);
-}
